f6e2a5dd3d80fe7433a06bcf2fb113226b898e62
Senhung Wong <alexwongsenhung@gmail.com>
2018-07-15 14:08:49 -0700

    Fix login body validation rule

--- app/Validators/AuthValidator.js
+++ app/Validators/AuthValidator.js
@@ -9,7 +9,7 @@ const login = {
     body: {
         username: Joi.string()
             .min(5).max(20)
-            .regex(/^[a-zA-Z][a-zA-Z0-9]*$/)
+            .regex(/^[a-zA-Z][a-zA-Z0-9_.]*$/)
             .required(),
 
         password: Joi.string()
