84a751f16f5f2bc07b9dbcf64d9e77724049dc5e
Senhung Wong <alexwongsenhung@gmail.com>
2018-07-25 08:08:59 -0700

    Update mongodb connection set up

--- configs/database.js
+++ configs/database.js
@@ -2,18 +2,10 @@ const env = process.env;
 
 module.exports = {
     timeline: {
-        host    : env.TIMELINE_DB_HOST     || 'localhost',
-        user    : env.TIMELINE_DB_USER     || null,
-        password: env.TIMELINE_DB_PASSWORD || null,
-        port    : env.TIMELINE_DB_PORT     || 27017,
-        database: env.TIMELINE_DB_DATABASE || 'timeline',
+        connection: env.MONGODB_URI || 'mongodb://localhost:27017/timeline'
     },
 
     testing: {
-        host    : env.TEST_DB_HOST     || 'localhost',
-        user    : env.TEST_DB_USER     || null,
-        password: env.TEST_DB_PASSWORD || null,
-        port    : env.TEST_DB_PORT     || 27017,
-        database: env.TEST_DB_DATABASE || 'timeline_testing',
+        connection: env.MONGODB_TEST_URI || 'mongodb://localhost:27017/timeline_test'
     },
 };
